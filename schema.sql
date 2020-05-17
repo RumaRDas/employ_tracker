9913ccef0450f3f9a4c3461eec6e334b6e80b10b
Ruma Das <rumaadl16@gmail.com>
2020-05-18 00:05:10 +0930

    adding all npm and database

---
+++ schema.sql
@@ -0,0 +1,39 @@
+BEGIN;
+
+-- Trash the db and create it from scratch.
+DROP DATABASE IF EXISTS employee_db;
+-- Creating database
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT auto_increment NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+)ENGINE=INNODB;
+
+CREATE TABLE  roles(
+    id INTEGER auto_increment NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(7,2),
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+
+)ENGINE=INNODB;
+CREATE TABLE employee (
+    id INTEGER auto_increment NOT NULL,
+    first_name  VARCHAR(30) NOT NULL,
+    last_name  VARCHAR(30) NOT NULL,
+    roles_id INT ,
+    manager_id int,
+    PRIMARY KEY (id),
+    FOREIGN KEY (roles_id)
+    REFERENCES roles(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES roles(id)
+) ENGINE=INNODB;
+
+COMMIT;
---
+++ server.js
@@ -0,0 +1,42 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const utils = require('util');
+
+// DB connection for this session.
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '2020',
+    database: 'employee_db'
+
+});
+
+// Wrap connection.connect() in a promise!
+async function connect(){
+    return new Promise((resolve, reject)=>{
+        connection.connect(err =>{
+            if (err) reject(err); //Connection Failed
+            else resolve(); //Connection Succeseed
+        });
+    });
+}
+
+// Wrap connection.query() in a promise!
+async function query(command, values){
+    return new Promise((resolve, reject)=>{
+        connection.query(command,values,(err, res)=>{
+            if(err) reject(err);
+            else resolve(res);
+        });
+    });
+}
+
+// This is where everything happens.
+async function main(){
+    await connect();
+    console.log("connected!", connection.threadId);
+
+
+}
+// Start the app.
+main().catch(err=> console.log(err));
