6a4a4d3f05bb5c8087d70cac22a3cde4831e471c
Ruma Das <rumaadl16@gmail.com>
2020-05-19 14:46:02 +0930

    changing server.js

--- server.js
+++ server.js
@@ -16,3 +16,30 @@ connection.connect(function(err) {
     console.log("connected as id " + connection.threadId);
  
   });
+
+  let listDepartment;
+  let listRoles;
+  let listEmployee;
+  
+  connection.query("SELECT * FROM roles", function(err, res) {
+    if (err) throw err;
+    listRoles = res.map(role => ({ name: role.title, value: role.id }));
+    console.log("Roles")
+    console.table(listRoles);
+  });
+  connection.query("SELECT * FROM department", function(err, res) {
+    if (err) throw err;
+    listDepartment = res.map(dep => ({ name: dep.name, value: dep.id }));
+    console.log("Department")
+    console.table(listDepartment);
+  });
+  
+  connection.query("SELECT * FROM employees", function(err, res) {
+    if (err) throw err;
+    listEmployee = res.map(emp => ({
+      name: `${emp.first_name}${emp.last_name}`,
+      value: emp.id
+    }));
+    console.log("Employee")
+    console.table(listEmployee);
+  });
